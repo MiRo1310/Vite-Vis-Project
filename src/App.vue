98dc58fea7f60a88b5fb9470227a808fce1e981e
MiRo1310 <michael.roling@gmx.de>
2024-03-03 13:56:21 +0100

    Socket connection established

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,14 @@
 <script setup lang="ts">
 import DataTable from '@/components/DataTable.vue';
 import { columns } from '@/components/columns.ts';
-import { Connection } from '@iobroker/socket-client';
+import { onMounted, ref } from 'vue';
+import { loadScript, init, IOBROKER_ADMIN_PORT, IOBROKER_HOST } from '@/lib/iobroker-connecter';
 
 
 
-
-// Konfigurationswerte
-const IOBROKER_HOST = '192.168.1.81';
-const IOBROKER_ADMIN_PORT = '8081';
-const IOBROKER_WS_PORT = '8084';
-
-const addScript = () => {
-  const scriptElement = document.createElement('script');
-  scriptElement.src = `http://${IOBROKER_HOST}:${IOBROKER_ADMIN_PORT}/lib/js/socket.io.js`;
-  document.head.appendChild(scriptElement);
-};
-
-const init = async () => {
-  const adminConnection = new Connection({
-    protocol: 'ws',
-    host: IOBROKER_HOST,
-    port: IOBROKER_WS_PORT,
-    admin5only: false,
-    autoSubscribes: [],
-  });
-
-  await adminConnection.startSocket();
-  await adminConnection.waitForFirstConnection();
-  console.log(await adminConnection.getEnums());
-  console.log(await adminConnection.getStates());
-  adminConnection.subscribeStateAsync('0_userdata.0.Testdatenpunkte.Test', (id: string, state: any) => {
-    console.log('State changed', id, state);
-  });
-};
-
-
-addScript(); // Fügen Sie das Skript hinzu, wenn die Komponente initialisiert wird
-init(); // Initialisieren Sie den Dienst
-
+onMounted(async () => {
+  loadScript(`http://${IOBROKER_HOST}:${IOBROKER_ADMIN_PORT}/lib/js/socket.io.js`, init);
+});
 
 const data = [{ amount: 1000, user: "" }, { amount: 1000, user: "" }, { amount: 1000, user: "" }, { amount: 1000, user: "Michael" }]
 
---
+++ src/lib/iobroker-connecter.ts
@@ -0,0 +1,37 @@
+import { Connection } from "@iobroker/socket-client";
+import { ref } from "vue";
+// Konfigurationswerte
+export const IOBROKER_HOST = "192.168.1.81";
+export const IOBROKER_ADMIN_PORT = "8081";
+const IOBROKER_WS_PORT = "8084";
+
+interface SocketObject {
+  [key: string]: any;
+}
+
+let adminConnection = ref<null | SocketObject>(null);
+export function loadScript(src: string, callback: any) {
+  let script = document.createElement("script");
+  script.src = src;
+  script.onload = () => callback();
+  document.body.appendChild(script);
+}
+
+export async function init() {
+  adminConnection.value = new Connection({
+    protocol: "ws",
+    host: IOBROKER_HOST,
+    port: IOBROKER_WS_PORT,
+    admin5only: false,
+    autoSubscribes: [],
+  });
+  if (adminConnection.value) {
+    await adminConnection.value.startSocket();
+    await adminConnection.value.waitForFirstConnection();
+    console.log(await adminConnection.value.getEnums());
+    console.log(await adminConnection.value.getStates());
+    adminConnection.value.subscribeStateAsync("0_userdata.0.Testdatenpunkte.Test", (id: string, state: any) => {
+      console.log("State changed", id, state);
+    });
+  }
+}
