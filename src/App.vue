6e17441c6b31d9f960dc70b4f10101edd8d1782c
MiRo1310 <michael.roling@gmx.de>
2024-03-23 12:15:08 +0100

    Add taillwind ui to app

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,90 @@
-<script setup lang="ts">
+<script setup>
 import NavLeft from './pages/NavLeft.vue';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { loadScript, init, IOBROKER_ADMIN_PORT, IOBROKER_HOST } from '@/lib/iobroker-connecter';
+import {
+  Dialog,
+  DialogPanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  TransitionChild,
+  TransitionRoot,
+} from '@headlessui/vue'
+import { Button } from './components/ui/button';
+import { MenuIcon, X } from 'lucide-vue-next';
 
 
 onMounted(async () => {
   loadScript(`http://${IOBROKER_HOST}:${IOBROKER_ADMIN_PORT}/lib/js/socket.io.js`, init);
 });
+
+
+
+
+
+const sidebarOpen = ref(false)
 </script>
-
-
 <template>
-  <div class="w-full h-[100vh] absolute -z-10 background">
 
-    <div class="w-full h-[100vh] flex flex-row">
-      <div class="nav--left ">
+  <div>
+    <TransitionRoot as="template" :show="sidebarOpen">
+      <Dialog as="div" class="relative z-50 lg:hidden" @close="sidebarOpen = false">
+        <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0"
+          enter-to="opacity-100" leave="transition-opacity ease-linear duration-300" leave-from="opacity-100"
+          leave-to="opacity-0">
+          <div class="fixed inset-0 bg-gray-900/80" />
+        </TransitionChild>
+
+        <div class="fixed inset-0 flex">
+          <TransitionChild as="template" enter="transition ease-in-out duration-300 transform"
+            enter-from="-translate-x-full" enter-to="translate-x-0"
+            leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0"
+            leave-to="-translate-x-full">
+            <DialogPanel class="relative mr-16 flex w-full max-w-xs flex-1">
+              <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0"
+                enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100" leave-to="opacity-0">
+                <div class="absolute left-full top-0 flex w-16 justify-center pt-5">
+                  <Button :size="'icon'" class="-m-2.5 p-2.5" @click="sidebarOpen = false">
+                    <X class="h-6 w-6" />
+                  </Button>
+                </div>
+              </TransitionChild>
+              <!-- Sidebar component, swap this element with another sidebar if you like -->
+              <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 pb-4">
+                <NavLeft />
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </Dialog>
+    </TransitionRoot>
+
+    <!-- Static sidebar for desktop -->
+    <div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-52 lg:flex-col">
+      <!-- Sidebar component, swap this element with another sidebar if you like -->
+      <div class="flex grow flex-col gap-y-5 overflow-y-auto background  pb-4">
         <NavLeft />
       </div>
-      <div class="p-1  w-full h-full">
-        <div>
-          <router-view />
+    </div>
+
+    <div class="lg:pl-52 background min-h-[100vh]">
+
+      <Button :size="'icon'" class="-m-2.5 p-2.5 text-gray-700 lg:hidden" @click="sidebarOpen = true">
+        <MenuIcon class="h-6 w-6" aria-hidden="true" />
+      </Button>
+
+
+      <main>
+        <div class="pr-2">
+          <RouterView />
         </div>
-      </div>
+      </main>
     </div>
   </div>
 </template>
-
 <style lang="postcss" scoped>
-.app--container {
-  @apply flex flex-row
-}
-
-.nav--left {
-  @apply w-[12rem]
-}
-
 .background {
   background-color: #0a1a58;
 }
