87f2df94797d85f1f51b8653f6dea77672827d6a
MiRo1310 <michael.roling@gmx.de>
2025-06-16 18:24:49 +0200

    FEAT: add diagram

--- src/pages/diagrams.vue
+++ src/pages/diagrams.vue
@@ -5,7 +5,9 @@ import { Button } from "@/components/shared/button";
 
 const diagrams: string[] = [
   "http://192.168.1.4:3000/d/feobdyuntkhs0d/temperaturen?orgId=1&from=now-24h&to=now&refresh=30s&kiosk",
-  "http://192.168.1.4:3000/d/de48h6zf94jcwe/pv-fuer-vis?orgId=1&refresh=10s&kiosk"
+  "http://192.168.1.4:3000/d/de48h6zf94jcwe/pv-fuer-vis?orgId=1&refresh=10s&kiosk",
+  "http://192.168.1.4:3000/d/aep1w0j2gjsowd/pool-temperatur?orgId=1&kiosk"
+
 ];
 
 const diagramIndex = ref(0);
