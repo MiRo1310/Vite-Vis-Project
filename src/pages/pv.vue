a28577f24f29f02c6dbc2d070812c3f379c95c58
MiRo1310 <michael.roling@gmx.de>
2025-06-19 13:29:46 +0200

    FEAT: fix some problema

--- src/pages/pv.vue
+++ src/pages/pv.vue
@@ -9,21 +9,19 @@ import {
 <template>
   <Card class="ml-0 h-full" styling="blue">
     <CardHeader>
-      <CardTitle> Strom </CardTitle>
+      <CardTitle> Strom</CardTitle>
     </CardHeader>
     <CardContent class="flex flex-wrap">
       <iframe
         src="http://192.168.1.4:3000/d-solo/Biuksv-4k/strom?orgId=1&panelId=2"
         width="750"
         height="300"
-        frameborder="0"
       />
       <iframe
         class="min-w-1/2"
         src="http://192.168.1.4:3000/d-solo/Biuksv-4k/strom?orgId=1&refresh=5s&panelId=13"
         width="750"
         height="300"
-        frameborder="0"
       />
     </CardContent>
   </Card>
