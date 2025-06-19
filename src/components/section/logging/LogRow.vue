afe49e3ce3635e13b7b02f59fddeeef07223f890
MiRo1310 <68854794+MiRo1310@users.noreply.github.com>
2025-06-19 15:31:20 +0200

    feature/Diagramms (#7)
    * FEAT: Add diagrams and fixes
    
    * FEAT: Fixes
    
    * FEAT: Update vue use
    
    * FEAT: Update vue and vue router
    
    * FEAT: Run lint and eslint plugin vue
    
    * FEAT: Install eslint
    
    * FEAT: iobroker types
    
    * FEAT: update iobroker/socket-client and eslint fixes
    
    * FEAT: update @typescript-eslint/eslint-plugin && luicide
    
    * FEAT: update vite
    
    * FEAT: update @vitejs/plugin-vue && types/node
    
    * FEAT: update tailwindcss
    
    * FEAT: update tailwind-merge
    
    * FEAT: update pinia
    
    * FEAT: update @types/node
    
    * FEAT: add diagram
    
    * FEAT: update postcss
    
    * FEAT: Refactor
    
    * FEAT: Refactor
    
    * FEAT: Add prettier
    
    * FEAT: fix some problema

---
+++ src/components/section/logging/LogRow.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { Log } from "@/types/types.ts";
+import { toLocaleTime } from "@/lib/time.ts";
+
+defineProps<{ log: Log }>();
+</script>
+
+<template>
+  <div class="log-row">
+    <span class="log-row__ts">{{ toLocaleTime(log.ts) }}</span>
+    <span class="log-row__from">{{ log.from }}</span>
+    {{ log.message }}
+  </div>
+</template>
+
+<style scoped lang="scss">
+.log-row {
+  @apply text-2xs;
+
+  &__ts {
+    @apply w-28 inline-block;
+  }
+
+  &__from {
+    @apply inline-block w-24;
+  }
+}
+</style>
---
+++ src/components/shared/page/Page.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps<{ title: string }>();
+</script>
+
+<template>
+  <div class="page">
+    <div class="page__title">
+      {{ title }}
+    </div>
+
+    <slot name="header" />
+
+    <div class="page__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.page {
+  @apply border bg-gray-200 text-card-foreground shadow overflow-hidden h-full p-2;
+  @apply flex flex-col;
+
+  &__title {
+    @apply text-color__default text-base font-bold bg-accent px-2;
+  }
+
+  &__content {
+    @apply flex-1 overflow-y-auto mt-2;
+  }
+}
+</style>
