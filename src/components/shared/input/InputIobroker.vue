6e1950bc4c59af6faab7e4912474fad913758dbf
MiRo1310 <michael.roling@gmx.de>
2025-07-03 06:51:30 +0200

    FEAT: FIX input

--- src/components/shared/input/InputIobroker.vue
+++ src/components/shared/input/InputIobroker.vue
@@ -1,23 +1,19 @@
 <script lang="ts" setup>
 import { Input } from "@/components/ui/input";
-import { InputShadcn } from "@/components/ui/input";
 import { StoreValue } from "@/store/iobrokerStore.ts";
 import { useDebounceFn } from "@vueuse/core";
 import { adminConnection } from "@/lib/connecter-to-iobroker.ts";
 import { watch } from "vue";
-import { InputType } from "@/components/ui/input/InputShadcn.vue";
-import { getVariantsClasses } from "@/composables/variants-classes.ts";
 
 const props = withDefaults(
   defineProps<{
-    type?: InputType;
-    unit?: "°C" | "Wh" | "%" | "min";
+    type?: "number" | "text";
+    unit?: "°C" | "Wh" | "%";
     state: StoreValue<string | number>;
     debounce?: number;
-    ack?: boolean;
-    color?: keyof typeof variants.color;
+    setAck?: boolean;
   }>(),
-  { type: "number", unit: undefined, debounce: 500, color: "default" },
+  { type: "number", unit: undefined, debounce: 1000 },
 );
 
 const modelValue = defineModel<string | number>();
@@ -37,20 +33,18 @@ const setState = () => {
   if (!modelValue.value || !id) {
     return;
   }
-  adminConnection?.setState(id, modelValue.value, props.ack);
-};
-
-const variants = {
-  color: {
-    default: "",
-    white: "bg-white",
-  },
+  adminConnection?.setState(id, modelValue.value, props.setAck);
 };
 </script>
 
 <template>
   <div :class="['input-with-unit', { 'input-with-unit__input--not-ack': !state?.ack }]">
-    <Input :type :class="['input-with-unit__input']" v-model:model-value="modelValue" @update:model-value="debounceFn" />
+    <Input
+      :type
+      class="h-6 border-2 border-transparent shadow-none w-12 focus:border-none active:border-none ring-0 text-right focus:ring-0 mr-4"
+      v-model:model-value="modelValue"
+      @update:model-value="debounceFn"
+    />
     <span class="input-with-unit__unit">{{ unit }}</span>
   </div>
 </template>
@@ -60,8 +54,6 @@ const variants = {
   @apply relative flex items-center border-b-color__default border-b-2;
 
   &__input {
-    @apply h-6 border-2 border-transparent shadow-none w-16 focus:border-none active:border-none ring-0 text-right focus:ring-0 pr-4;
-
     &--not-ack {
       @apply border-red-500;
     }
@@ -69,7 +61,7 @@ const variants = {
 
   &__unit {
     @apply ml-1 pr-1 text-accent-foreground/50 font-bold text-xs;
-    @apply absolute right-0;
+    @apply absolute right-3;
   }
 }
 </style>
