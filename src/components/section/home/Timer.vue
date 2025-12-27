26dc5cfef76c93886b07d28c3a7b640e20215486
MiRo1310 <68854794+MiRo1310@users.noreply.github.com>
2025-12-27 08:58:48 +0100

    FEAT: Timer name in header (#23)

--- src/components/section/home/Timer.vue
+++ src/components/section/home/Timer.vue
@@ -2,7 +2,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/shared/card";
 import { useIobrokerStore } from "@/store/iobrokerStore.ts";
 import { storeToRefs } from "pinia";
-import { HTMLAttributes } from "vue";
+import { computed, HTMLAttributes } from "vue";
 import { useAppStore } from "@/store/appStore.ts";
 import { Timer, Timers } from "@/types/types.ts";
 
@@ -11,6 +11,11 @@ defineProps<{ class: HTMLAttributes["class"] }>();
 const iobrokerStore = useIobrokerStore();
 
 const { timer } = storeToRefs(iobrokerStore);
+
+const nameByIndex = computed(() => (index: number) => {
+  const timerName = (timer.value[index as keyof Timers] as Timer).name?.val;
+  return timerName != "Timer" ? timerName : null;
+});
 </script>
 
 <template>
@@ -21,28 +26,22 @@ const { timer } = storeToRefs(iobrokerStore);
       </CardTitle>
     </CardHeader>
     <CardContent class="timer__content">
-      <div v-for="i in 4" :key="i" class="min-w-[180px] flex-1 max-w-1/2 m-1 flex px-2 default_card">
-        <div class="w-full">
+      <div v-for="i in 4" :key="i" class="timer__section default_card">
+        <div class="timer__section-inner">
           <h1 class="timer__sub-title line">
-            <span>Timer {{ i }}</span>
+            <span>{{ nameByIndex(i) || `Timer ${i}` }}</span>
             <span> {{ (timer[i as keyof Timers] as Timer).timeString?.val }}</span>
           </h1>
-          <div class="w-full text-xs">
-            <div class="timer__label text-base">
-              <p>Name:</p>
-              <p class="flex-1 text-right">
-                {{ (timer[i as keyof Timers] as Timer).name?.val }}
-              </p>
-            </div>
+          <div class="timer__values">
             <div class="timer__label">
               <p>Gerät:</p>
-              <p class="flex-1 text-right">
+              <p class="timer__value">
                 {{ (timer[i as keyof Timers] as Timer).device?.val }}
               </p>
             </div>
             <div class="timer__label">
               <p>Länge:</p>
-              <p class="flex-1 text-right">
+              <p class="timer__value">
                 {{ (timer[i as keyof Timers] as Timer).initialTimer?.val }}
               </p>
             </div>
@@ -59,6 +58,14 @@ const { timer } = storeToRefs(iobrokerStore);
     @apply flex flex-wrap -mx-1 min-w-[400px];
   }
 
+  &__section {
+    @apply min-w-[180px] flex-1 max-w-[50%] m-1 flex px-2;
+  }
+
+  &__section-inner {
+    @apply w-full;
+  }
+
   &__sub-title {
     @apply text-gray-500 text-xl;
     @apply flex justify-between;
@@ -67,6 +74,14 @@ const { timer } = storeToRefs(iobrokerStore);
   &__label {
     @apply text-accent-foreground/50 font-bold;
     @apply flex justify-between;
+  }
+
+  &__values {
+    @apply w-full text-xs;
+  }
+
+  &__value {
+    @apply flex-1 text-right;
   }
 }
 </style>
