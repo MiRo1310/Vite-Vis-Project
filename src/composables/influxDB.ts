290cf3a598746b4bc1bcf5687d89feec965df711
MiRo1310 <michael.roling@gmx.de>
2026-01-24 14:40:57 +0100

    feat: Refactor heating components and enhance InfluxDB client for multi-measurement support

--- src/composables/influxDB.ts
+++ src/composables/influxDB.ts
@@ -18,7 +18,7 @@ export class InfluxDBClient {
   private readonly org = import.meta.env.VITE_INFLUX_DB_ORG as string;
   private readonly bucket: string = "iobroker";
   private readonly intervallSec: number;
-  private readonly measurement;
+  private readonly measurement: string[];
   private readonly range: number | null = null;
   private readonly intervalLoad: boolean = false;
   private readonly valueType: "boolean" | "number" = "number";
@@ -32,7 +32,7 @@ export class InfluxDBClient {
   private api: QueryApi;
 
   constructor(
-    measurement: string,
+    measurement: string[],
     options?: { type?: "boolean" | "number"; rangeSec?: number; intervall?: number; bucket?: string; url?: string; token?: string; org?: string },
   ) {
     if (options) {
@@ -45,7 +45,7 @@ export class InfluxDBClient {
       this.valueType = options.type ?? "number";
     }
 
-    this.measurement = `"${measurement}"`;
+    this.measurement = measurement;
     this.intervallSec = options?.intervall ?? 60;
     this.api = new InfluxDB({ url: this.url, token: this.token }).getQueryApi(this.org);
   }
@@ -57,11 +57,10 @@ export class InfluxDBClient {
     }
     this.api.queryRows(this.getQuery(), {
       next: (row: string[], tableMeta: FluxTableMetaData) => {
-        console.log(row, tableMeta);
         const o = tableMeta.toObject(row);
-        console.log(o);
+        // console.log(o);
         // console.log(tableMeta.get(row, "_value"));
-        this.resultTemp.push({ time: o._time, value: o._value });
+        this.resultTemp.push({ time: o._time, [o._measurement]: o._value ? 1 : 0 });
       },
       error: (error: Error) => {
         logging({ e: error, title: "QueryRows ERROR", type: "error" });
@@ -82,10 +81,15 @@ export class InfluxDBClient {
       return this.fluxQuery();
     }
   }
+
+  private buildMeasurement() {
+    return this.measurement.map((m) => `r["_measurement"] == "${m}"`).join(" or ");
+  }
+
   private fluxQuery() {
     return `from(bucket: "${this.bucket}")
   |> range(start: ${this.start}, stop: ${this.stop})
-  |> filter(fn: (r) => r["_measurement"] == ${this.measurement})
+  |> filter(fn: (r) => ${this.buildMeasurement()})
   |> filter(fn: (r) => r["_field"] == "value")
   |> aggregateWindow(every: ${this.windowPeriod}, fn: mean, createEmpty: false)
   |> yield(name: "mean")`;
@@ -94,7 +98,7 @@ export class InfluxDBClient {
   private fluxQueryBoolean() {
     return `from(bucket: "${this.bucket}")
   |> range(start: ${this.start}, stop: ${this.stop})
-  |> filter(fn: (r) => r["_measurement"] == ${this.measurement})
+  |> filter(fn: (r) => ${this.buildMeasurement()})
   |> filter(fn: (r) => r["_field"] == "value") `;
   }
 
