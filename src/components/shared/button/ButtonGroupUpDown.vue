32d4e9d7609d773eefb91580e8cbd51b0fe14b04
MiRo1310 <michael.roling@gmx.de>
2026-03-26 18:07:40 +0100

    FEAT: Enhance responsive design across multiple components

--- src/components/shared/button/ButtonGroupUpDown.vue
+++ src/components/shared/button/ButtonGroupUpDown.vue
@@ -7,12 +7,12 @@ const emits = defineEmits(["up", "down"]);
 </script>
 
 <template>
-  <div class="w-6 flex flex-col items-center justify-center">
+  <div class="w-6 h-6 flex flex-col items-center justify-center">
     <Button type="button" variant="ghost" :disabled="disabledUp" size="iconRow" @click="emits('up')">
-      <ChevronUp class="size-4 cursor-pointer hover:bg-background/30 rounded-xs" />
+      <ChevronUp class="size-3 cursor-pointer hover:bg-background/30 rounded-xs" />
     </Button>
     <Button type="button" variant="ghost" :disabled="disabledDown" size="iconRow" @click="emits('down')">
-      <ChevronDown class="size-4 cursor-pointer hover:bg-background/30 rounded-xs" />
+      <ChevronDown class="size-3 cursor-pointer hover:bg-background/30 rounded-xs" />
     </Button>
   </div>
 </template>
