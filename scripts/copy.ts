5688fa50c652001a6ed0ce9325af569f8bc30fb9
MiRo1310 <michael.roling@gmx.de>
2026-03-30 15:08:23 +0200

    1.3.4

---
+++ scripts/copy.ts
@@ -0,0 +1,8 @@
+import { copyDataToRemote } from "./release.ts";
+import fs from "fs";
+
+// 2. Version holen (z. B. aus package.json)
+const pkg = JSON.parse(fs.readFileSync("package.json", "utf-8"));
+const version = pkg.version;
+
+copyDataToRemote(version);
--- scripts/release.ts
+++ scripts/release.ts
@@ -24,23 +24,23 @@ run("git push --follow-tags");
 const pkg = JSON.parse(fs.readFileSync("package.json", "utf-8"));
 const version = pkg.version;
 
-// Alternative: Git Commit Hash
-const commit = execSync("git rev-parse --short HEAD").toString().trim();
-
 // 4. ENV Datei schreiben
 const envContent = `VITE_APP_VERSION=${version}
-VITE_APP_COMMIT=${commit}
 `;
 
 fs.writeFileSync(".env.local", envContent);
 
-// Build mit injected Version
-run(`npx cross-env VITE_APP_VERSION=${version} VITE_APP_COMMIT=${commit} yarn build`);
+copyDataToRemote(version);
 
-// Kopieren
-const user = process.env.VITE_USERNAME;
-const host = process.env.VITE_HOST;
-const path = process.env.VITE_PATH;
+export function copyDataToRemote(version: string) {
+  // Build mit injected Version
+  run(`npx cross-env VITE_APP_VERSION=${version} yarn build`);
 
-run(`scp -r dist/* ${user}@${host}:${path}`);
-console.log(`✅ Release fertig! Version: ${version}`);
+  // Kopieren
+  const user = process.env.VITE_USERNAME;
+  const host = process.env.VITE_HOST;
+  const path = process.env.VITE_PATH;
+
+  run(`scp -r dist/* ${user}@${host}:${path}`);
+  console.log(`✅ Release fertig! Version: ${version}`);
+}
