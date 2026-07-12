ead93ba37205c645848e8d41a0fd24d9ddf4c581
MiRo1310 <68854794+MiRo1310@users.noreply.github.com>
2026-07-12 09:42:50 +0200

    feature/PWA (#314)
    * FEAT: PWA
    
    FEAT: update favicon.svg with new design and improved dimensions
    
    CHORE: remove unused tsconfig reference to tsconfig.app.json
    
    FEAT: enhance PWA configuration in vite.config.ts for improved functionality

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,45 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import svgLoader from "vite-svg-loader";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), vue(), svgLoader()],
+  plugins: [
+    tailwindcss(),
+    vue(),
+    svgLoader(),
+    !process.env.VITEST &&
+      VitePWA({
+        registerType: "autoUpdate",
+        injectRegister: false,
+
+        pwaAssets: {
+          disabled: false,
+          config: true,
+        },
+
+        manifest: {
+          name: "Smarthome",
+          short_name: "Smarthome",
+          description: "Visualization for smarthome",
+          theme_color: "#ffffff",
+        },
+
+        workbox: {
+          globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+          cleanupOutdatedCaches: true,
+          clientsClaim: true,
+        },
+
+        devOptions: {
+          enabled: false,
+          navigateFallback: "index.html",
+          suppressWarnings: true,
+          type: "module",
+        },
+      }),
+  ],
   resolve: {
     alias: {
       "@": "/src",
